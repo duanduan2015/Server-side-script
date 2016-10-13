a73a53407351b1a6c307646af03d7a5b3ba49701
Yunduan Han <yunduan.han@gmail.com>
2016-10-13 15:05:55 -0700

    View Details works

--- congress.php
+++ congress.php
@@ -7,6 +7,12 @@
 ?>
 <head>
 <script>
+    function displayDetails(index) {
+        var display = document.getElementById("display");
+        var id = "details".concat(index.toString());
+        var details = document.getElementById(id);
+        display.innerHTML = details.innerHTML;
+    }
     function keywordSelect() {
         var database = document.getElementById("database");
         var checked = database.selectedIndex;
@@ -137,58 +143,7 @@ fieldset {
         $keyword = $_POST["keyword"];
         if ($database == "legislators") {
             $keywordName = "state";
-            $states = array(
-                "Alabama"=>"AL",
-                "Alaska"=>"AK",
-                "Arizona"=>"AZ",
-                "Arkansas"=>"AR",
-                "California"=>"CA",
-                "Colorado"=>"CO",
-                "Connecticut"=>"CT",
-                "Delaware"=>"DE",
-                "Florida"=>"FL",
-                "Georgia"=>"GA",
-                "Hawaii"=>"HI",
-                "Idaho"=>"ID",
-                "Illinois"=>"IL",
-                "Indiana"=>"IN",
-                "Iowa"=>"IA",
-                "Kansas"=>"KS",
-                "Kentucky"=>"KY",
-                "Louisiana"=>"LA",
-                "Maine"=>"ME",
-                "Maryland"=>"MD",
-                "Massachusetts"=>"MA",
-                "Michigan"=>"MI",
-                "Minnesota"=>"MN",
-                "Mississippi"=>"MS",
-                "Missouri"=>"MO",
-                "Montana"=>"MT",
-                "Nebraska"=>"NE",
-                "Nevada"=>"NV",
-                "New Hampshire"=>"NH",
-                "New Jersey"=>"NJ",
-                "New Mexico"=>"NM",
-                "New York"=>"NY",
-                "North Carolina"=>"NC",
-                "North Dakota"=>"ND",
-                "Ohio"=>"OH",
-                "Oklahoma"=>"OK",
-                "Oregon"=>"OR",
-                "Pennsylvania"=>"PA",
-                "Rhode Island"=>"RI",
-                "South Carolina"=>"SC",
-                "South Dakota"=>"SD",
-                "Tennessee"=>"TN",
-                "Texas"=>"TX",
-                "Utah"=>"UT",
-                "Vermont"=>"VT",
-                "Virginia"=>"VA",
-                "Washington"=>"WA",
-                "West Virginia"=>"WV",
-                "Wisconsin"=>"WI",
-                "Wyoming"=>"WY",
-            );
+            $states = getStatesTable();
             $keyword = $states[$keyword];
             if ($keyword == null) {
                 $keyword = $_POST["keyword"];
@@ -219,15 +174,82 @@ fieldset {
             echo '<h2 align="center">' . $message . '</h2>';
         }
         $results = $decode["results"];
-        $table = '<table class="border" align="center" width="70%"><tr class="border"><th>Name</th><th class="border">State</th><th class="border">Chamber</th><th class="border">Details</th></tr>';
+        $table = '<div id="display"><table class="border" align="center" width="70%"><tr class="border"><th>Name</th><th class="border">State</th><th class="border">Chamber</th><th class="border">Details</th></tr>';
         for ($i = 0; $i < $decode["count"]; $i++) {
-            $newRow = '<tr class="border"><td  class="border">'.$results[$i]["first_name"] . ' ' . $results[$i]["last_name"] . '</td><td class="border">' . $results[$i]["state_name"] . '</td><td  class="border">' . $results[$i]["chamber"] . '</td></tr>';
+            $id = "bioguide_id=" . $results[$i]["bioguide_id"];
+            $link = $url . $database . "?" . $chamber . "&" . $keyword . "&" . $id . "&" . $apikey;
+            $details = file_get_contents($link, false, $context);
+            $detailsDecode = json_decode($details, true);
+            $GLOBALS[$i] = '<table class="border" width="70%" align="center"><tr><td>Name</td><td>Gender</td></tr></table>';
+            $detailsTable = '<div id="details' . $i . '" style="display:none;">' . $GLOBALS[$i] . '</div>';
+            echo $detailsTable;
+            $detailsLink = '<a href="javascript:displayDetails(' . $i . ');">View Details</a>';
+            $newRow = '<tr class="border"><td  class="border">'.$results[$i]["first_name"] . ' ' . $results[$i]["last_name"] . '</td><td class="border">' . $results[$i]["state_name"] . '</td><td  class="border">' . $results[$i]["chamber"] . '</td><td class="border">' . $detailsLink . '</td></tr>';
             $table = $table. $newRow;
         }
-        $table = $table . '</table>';
+        $table = $table . '</table></div>';
         echo $table;
         var_dump($decode["count"]);
         $_POST["submit"] = null;
+    }
+    function getDetailsTable($decode) {
+        $table = '<table class="border" width="70%" align="center"><tr><td>Name</td><td>Gender</td></tr></table>';
+        return $table;
+    }
+    function getStatesTable() {
+        $states = array(
+            "Alabama"=>"AL",
+            "Alaska"=>"AK",
+            "Arizona"=>"AZ",
+            "Arkansas"=>"AR",
+            "California"=>"CA",
+            "Colorado"=>"CO",
+            "Connecticut"=>"CT",
+            "Delaware"=>"DE",
+            "Florida"=>"FL",
+            "Georgia"=>"GA",
+            "Hawaii"=>"HI",
+            "Idaho"=>"ID",
+            "Illinois"=>"IL",
+            "Indiana"=>"IN",
+            "Iowa"=>"IA",
+            "Kansas"=>"KS",
+            "Kentucky"=>"KY",
+            "Louisiana"=>"LA",
+            "Maine"=>"ME",
+            "Maryland"=>"MD",
+            "Massachusetts"=>"MA",
+            "Michigan"=>"MI",
+            "Minnesota"=>"MN",
+            "Mississippi"=>"MS",
+            "Missouri"=>"MO",
+            "Montana"=>"MT",
+            "Nebraska"=>"NE",
+            "Nevada"=>"NV",
+            "New Hampshire"=>"NH",
+            "New Jersey"=>"NJ",
+            "New Mexico"=>"NM",
+            "New York"=>"NY",
+            "North Carolina"=>"NC",
+            "North Dakota"=>"ND",
+            "Ohio"=>"OH",
+            "Oklahoma"=>"OK",
+            "Oregon"=>"OR",
+            "Pennsylvania"=>"PA",
+            "Rhode Island"=>"RI",
+            "South Carolina"=>"SC",
+            "South Dakota"=>"SD",
+            "Tennessee"=>"TN",
+            "Texas"=>"TX",
+            "Utah"=>"UT",
+            "Vermont"=>"VT",
+            "Virginia"=>"VA",
+            "Washington"=>"WA",
+            "West Virginia"=>"WV",
+            "Wisconsin"=>"WI",
+            "Wyoming"=>"WY",
+        );
+        return $states;
     }
 ?>
 </body>
